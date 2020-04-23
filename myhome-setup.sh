16973cd69327499c73dc971751de3477f695ad4e
Thomas Hutterer <thutterer@suse.de>
2020-04-23 17:08:17 +0200

    Update myhome-setup.sh

--- myhome-setup.sh
+++ myhome-setup.sh
@@ -1,3 +1,6 @@
+# to install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # to enable zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
