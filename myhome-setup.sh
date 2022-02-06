f76f2e41a2e24433bc5c01516c7fcff566a11e5c
Thomas Hutterer <5372947+thutterer@users.noreply.github.com>
2022-02-06 13:46:13 +0100

    Remove all i3 related config

--- myhome-setup.sh
+++ myhome-setup.sh
@@ -5,12 +5,3 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 # to enable zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# for i3
-
-zypper install nitrogren # A background browser and setter for X windows
-zypper install redshift-gtk
-zypper install rxvt-unicode
-zypper install scrot     # A screenshot tool
-zypper install rofi
-sudo pip install py3status
