decb88eb37c906048be590b4846912eb1e2cc28d
Thomas Hutterer <thutterer@gitlab.com>
2023-03-20 13:20:08 +0100

    Document install step

--- myhome-setup.sh
+++ myhome-setup.sh
@@ -5,3 +5,6 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 # to enable zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# to enable fzf-tab completion
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
